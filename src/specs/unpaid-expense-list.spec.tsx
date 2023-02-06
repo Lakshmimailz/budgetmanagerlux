63ad923b20455a783fcf56fb093996c13d09e8bc
Lakshmi Ponnusamy <lakshmi_mailz@yahoo.com>
2023-02-06 11:30:23 -0500

    changed

--- src/specs/unpaid-expense-list.spec.tsx
+++ src/specs/unpaid-expense-list.spec.tsx
@@ -1,13 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { UnpaidExpenseList } from "../unpaid-expense-list";
 import { Expense} from  "../expense-reducer";
-/*
+
 test("unpaid expense list",async ()=>{
-    const unpaidExpenses: expense[]=[
-        {name:"Laundry", cost:500, isEssential=false},
-        {name:"Phone", cost:1000, isEssential=false}]
-        render(<UnpaidExpenseList unpaidExpenses={expense}/>)
+    const unpaidExpenses: Expense[]=[
+        {name:"Laundry", cost:500, isEssential:false},
+        {name:"Phone", cost:1000, isEssential:false}]
+
+        // create a dummy dispatch variable
+        let dispatch:any = null;
+
+        render(<UnpaidExpenseList unpaidExpenses={unpaidExpenses} dispatch = {dispatch}/>)
+
       const elementLaundry = await screen.findByText(/Laundry/);
       expect(elementLaundry.innerHTML).toBe("Laundry");
 })
-*/
+
